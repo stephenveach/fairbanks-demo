489c8269f7e1d5fb0c19ed5bd3c70d8f7b0cafd2
Stephen Veach <stephen@veach.dev>
2026-02-27 20:40:30 -0600

    Seeders and converters

--- database/seeders/StatamicContentSeeder.php
+++ database/seeders/StatamicContentSeeder.php
@@ -14,27 +14,62 @@ class StatamicContentSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->runImport('statamic:eloquent:import-sites');
-        $this->runImport('statamic:eloquent:import-blueprints', ['--force' => true, '--all-blueprints' => true]);
-        $this->runImport('statamic:eloquent:import-collections', ['--force' => true]);
-        $this->runImport('statamic:eloquent:import-taxonomies', ['--force' => true]);
-        $this->runImport('statamic:eloquent:import-navs', ['--force' => true]);
-        $this->runImport('statamic:eloquent:import-globals', ['--force' => true]);
-        $this->runImport('statamic:eloquent:import-assets', ['--force' => true]);
-        $this->runImport('statamic:eloquent:import-forms', ['--force' => true]);
-        $this->runImport('statamic:eloquent:import-addon-settings');
-        $this->runImport('statamic:eloquent:import-entries');
+        if ($this->usesEloquentDriver('sites')) {
+            $this->runImport('statamic:eloquent:import-sites');
+        }
+
+        if ($this->usesEloquentDriver('blueprints') || $this->usesEloquentDriver('fieldsets')) {
+            $this->runImport('statamic:eloquent:import-blueprints', ['--force' => true, '--all-blueprints' => true]);
+        }
+
+        if ($this->usesEloquentDriver('collections') || $this->usesEloquentDriver('collection_trees')) {
+            $this->runImport('statamic:eloquent:import-collections', ['--force' => true]);
+        }
+
+        if ($this->usesEloquentDriver('taxonomies') || $this->usesEloquentDriver('terms')) {
+            $this->runImport('statamic:eloquent:import-taxonomies', ['--force' => true]);
+        }
+
+        if ($this->usesEloquentDriver('navigations') || $this->usesEloquentDriver('navigation_trees')) {
+            $this->runImport('statamic:eloquent:import-navs', ['--force' => true]);
+        }
+
+        if ($this->usesEloquentDriver('global_sets') || $this->usesEloquentDriver('global_set_variables')) {
+            $this->runImport('statamic:eloquent:import-globals', ['--force' => true]);
+        }
+
+        if ($this->usesEloquentDriver('asset_containers') || $this->usesEloquentDriver('assets')) {
+            $this->runImport('statamic:eloquent:import-assets', ['--force' => true]);
+        }
+
+        if ($this->usesEloquentDriver('forms') || $this->usesEloquentDriver('form_submissions')) {
+            $this->runImport('statamic:eloquent:import-forms', ['--force' => true]);
+        }
+
+        if ($this->usesEloquentDriver('addon_settings')) {
+            $this->runImport('statamic:eloquent:import-addon-settings');
+        }
+
+        if ($this->usesEloquentDriver('entries')) {
+            $this->runImport('statamic:eloquent:import-entries');
+        }
 
         if ($this->canImportRoles()) {
             $this->runImport('statamic:eloquent:import-roles');
+        } else {
+            $this->warnSkip('statamic:eloquent:import-roles');
         }
 
         if ($this->canImportGroups()) {
             $this->runImport('statamic:eloquent:import-groups');
+        } else {
+            $this->warnSkip('statamic:eloquent:import-groups');
         }
 
         if ($this->canImportUsers()) {
             $this->runImport('statamic:eloquent:import-users');
+        } else {
+            $this->warnSkip('statamic:eloquent:import-users');
         }
     }
 
@@ -58,6 +93,11 @@ class StatamicContentSeeder extends Seeder
         }
     }
 
+    private function usesEloquentDriver(string $key): bool
+    {
+        return config("statamic.eloquent-driver.{$key}.driver", 'file') === 'eloquent';
+    }
+
     private function canImportRoles(): bool
     {
         return (bool) config('statamic.users.tables.roles', false);
@@ -79,5 +119,12 @@ class StatamicContentSeeder extends Seeder
         $model = config("auth.providers.{$provider}.model");
 
         return is_string($model) && in_array(HasUuids::class, class_uses_recursive($model), true);
+    }
+
+    private function warnSkip(string $command): void
+    {
+        if ($this->command !== null) {
+            $this->command->warn("Skipping {$command} due to current Statamic user/auth config.");
+        }
     }
 }
